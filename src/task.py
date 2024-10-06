5fc9c52681998b3494a63bfbb53813877b62b6d8
turing <(none)>
2024-10-06 22:06:27 +0530

    MODIFY: Set autocommit

--- src/task.py
+++ src/task.py
@@ -11,15 +11,14 @@ POSTGRES_URI = str(getenv('POSTGRES_URI'))
 
 celery_app = Celery(main="saveres", broker=f"{REDIS_URI}/{REDIS_BROKER_DBINDEX}", backend=f"{REDIS_URI}/{REDIS_BACKEND_DBINDEX}")
 db_conn = psycopg.connect(POSTGRES_URI)
+db_conn.set_autocommit(True)
 
 @celery_app.task
 def save_res_to_db(url, ctx, content):
     with db_conn.cursor() as cur:
         cur.execute("""INSERT INTO res (url, ctx, content) VALUES (%s, %s, %s);""", (url, Json(ctx), content))
-        db_conn.commit()
 
 @celery_app.task
 def save_err_to_db(url, ctx, error):
     with db_conn.cursor() as cur:
         cur.execute("""INSERT INTO err (url, ctx, error) VALUES (%s, %s, %s);""", (url, Json(ctx), error))
-        db_conn.commit()
